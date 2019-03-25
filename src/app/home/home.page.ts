cc51c4cfad8aed4264548b52fe5e06bbaeb62083
jonivesto <n7vejo00@students.oamk.fi>
2019-03-25 15:09:29 +0200

    Working example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+/**
+ * @author Joni-Pekka Vesto <https://www.jonivesto.com/>
+ * @since 23-03-2019
+ */
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private storage: Storage) { }
+
+  // Runs on page load
+  ngOnInit(){
+    // Save to storage
+    this.storage.set('my_key', 'my_value');
+
+    // Load from storage
+    this.storage.get('my_key').then((value) => {
+      console.log('value for my_key:', value);
+    });
+  }
 
 }
